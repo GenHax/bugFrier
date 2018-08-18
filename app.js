076dafb9dce88fa6cb0aba7c09ef0b821cfccd5b
sudonitesh <nktosni@gmail.com>
2018-08-18 20:08:03 +0530

    improved project structure

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 const expressValidator = require('express-validator');
-var flash = require('connect-flash');
+const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local'),Strategy;
 const mongo = require('mongodb');
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/elearn');
+const db = mongoose.connection;
+async = require('async');
 
-mongoose.connect('mongodb://localhost:27017/waste-management');
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-
-var db = mongoose.connection; 
-
-//define router source
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const solidWaste = require('./routes/solidWaste');
+const localUser = require('./routes/localUser');
+const localAgent = require('./routes/localAgent');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-
-//body-parser mideleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-//cookie parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-//static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//express session
+// Express Session
 app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
 }));
 
-//passport
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-//express validator-------------------------
+
+// Express Validator
 app.use(expressValidator({
   errorFormatter: function(param, msg, value) {
       var namespace = param.split('.')
@@ -68,12 +67,12 @@ app.use(expressValidator({
   }
 }));
 
-//connect flash
+// Connect-Flash
 app.use(flash());
 
-//make the user object global in all views
-app.get('*', function(req, res, next){
-  //put user into res.locals for easy access form templates
+// Makes the user object global in all views
+app.get('*', function(req, res, next) {
+  // put user into res.locals for easy access from templates
   res.locals.user = req.user || null;
   if(req.user){
     res.locals.type = req.user.type;
@@ -81,33 +80,49 @@ app.get('*', function(req, res, next){
   next();
 });
 
-//global vars
+
+// Global Vars
 app.use(function (req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
-  res.locals.error_error = req.flash('error');
-
+  res.locals.error = req.flash('error');
   next();
 });
 
-//use router
-app.use('/', indexRouter);
-app.use('/user', userRouter);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use('/', index);
+app.use('/users', users);
+app.use('/solidWaste', solidWaste);
+app.use('/localUser', localUser);
+app.use('/localAgent', localAgent);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 
 module.exports = app;
